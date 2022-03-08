c3b64ddf9085bf1e88c9b2b5b706a4f993858110
Azodox <likemoi99@gmail.com>
2022-03-08 13:21:47 +0100

    Add click listener

---
+++ js/onclick.js
@@ -0,0 +1,7 @@
+var calculateButton = document.getElementById('calculate-button')
+var delivery = document.getElementById('delivery')
+var discount = document.getElementById('discount')
+
+calculateButton.addEventListener('click', function() {
+    console.log(delivery.nodeValue)
+})
