56958ba23f5beaa00a5bee1a97dd888d2c64e481
Azodox_ <likemoi99@gmail.com>
2022-04-29 21:39:20 +0200

    Revert changing chocochoco result name.

--- js/calculate.js
+++ js/calculate.js
@@ -17,7 +17,7 @@ var tip = document.getElementById('tip-amount')
 var data = '{"drinks":[{"iced_coffee":{"normal":50,"delivery":60},"vanilla_iced_coffee":{"normal":65,"delivery":75},"caramel_iced_coffee":{"normal":65,"delivery":75},"coffee":{"normal":40,"delivery":50},"decaffeinated_coffee":{"normal":40,"delivery":50},"cappuccino_coffee":{"normal":50,"delivery":60},"latte_coffee":{"normal":45,"delivery":55},"americano":{"normal":45,"delivery":55},"moka":{"normal":50,"delivery":60},"hot_chocolate":{"normal":55,"delivery":65},"chocolatine":{"normal":45,"delivery":55},"croissant":{"normal":35,"delivery":45},"muffins":{"normal":40,"delivery":50}, "gfingsoif":{"normal":135,"delivery":150},"chocochoco":{"normal":100,"delivery":120},"lebonmatin":{"normal":75,"delivery":95},"macncheese":{"normal":100,"delivery":120}}]}'
 var prices = JSON.parse(data)
 
-var simplifiedData = '{"coffee":"c","decaffeinated_coffee":"cd","cappuccino_coffee":"ca","latte_coffee":"cl","americano":"a","moka":"m","hot_chocolate":"cc","iced_coffee":"cg","vanilla_iced_coffee":"cgv","caramel_iced_coffee":"cgc","chocolatine":"ch","croissant":"cr","muffins":"mf","gfingsoif":" G fin G soif","chocochoco":" Chocochoco","lebonmatin":" Le bon matin","macncheese":"mac"}'
+var simplifiedData = '{"coffee":"c","decaffeinated_coffee":"cd","cappuccino_coffee":"ca","latte_coffee":"cl","americano":"a","moka":"m","hot_chocolate":"cc","iced_coffee":"cg","vanilla_iced_coffee":"cgv","caramel_iced_coffee":"cgc","chocolatine":"ch","croissant":"cr","muffins":"mf","gfingsoif":" G fin G soif","chocochoco":" Choco\'choco","lebonmatin":" Le bon matin","macncheese":"mac"}'
 var simplified = JSON.parse(simplifiedData)
 
 calculate.addEventListener('click', function(e) {
