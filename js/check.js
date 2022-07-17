76c971d46c565166170f4a4b41c557a5096939ae
Azodox_ <likemoi99@gmail.com>
2022-07-17 09:22:14 +0200

    If 10-10 is checked, disable every other option and Pokémon container.

--- js/check.js
+++ js/check.js
@@ -8,60 +8,90 @@ var delivery = document.getElementById("delivery")
 var company = document.getElementById("company")
 var partnership = document.getElementById("partnership")
 var publicService = document.getElementById("public-service")
+var tenten = document.getElementById("10-10")
 var drinksContainer = Array.from(document.getElementById('drinks').children)
+var menuContainer = Array.from(document.getElementById('menu-container').children)
 
 function onCheck(element){
-    if(element.id == "partnership"){
-        if(partnership.checked){
-            company.disabled = true
-            delivery.disabled = true
-            publicService.disabled = true
-        }else{
-            company.disabled = false
-            delivery.disabled = false
-            publicService.disabled = false
-        }
-    }else if(element.id == "company"){
-        partnership.checked = false
-        publicService.checked = false
-    }else if(element.id == "public-service"){
-        if(publicService.checked){
-            company.disabled = true
-            delivery.disabled = true
-            partnership.disabled = true
-            drinksContainer.forEach(function(drinks) {
-                if(drinks.children){
-                    Array.from(drinks.children).forEach(function(drink) {
-                        if(drink.children){
-                            Array.from(drink.children).forEach(function(d){
-                                if(d.tagName == 'INPUT'){
-                                    if(d.parentElement.id != "coffee-container" && d.parentElement.id != "croissant-container" && d.parentElement.id != "iced-coffee-container" && d.parentElement.id != "chocolatine-container"){
-                                        d.disabled = true
-                                    }    
-                                }
-                            })
-                        }
-                    })
-                }
+    switch(element.id){
+        case "partnership":
+            if(partnership.checked){
+                company.disabled = true
+                delivery.disabled = true
+                publicService.disabled = true
+                tenten.disabled = true
+            }else{
+                company.disabled = false
+                delivery.disabled = false
+                publicService.disabled = false
+                tenten.disabled = false
+            }
+            break;
+
+        case "company":
+            partnership.checked = false
+            publicService.checked = false
+            break;
+        
+        case "public-service":
+            if(publicService.checked){
+                company.disabled = true
+                delivery.disabled = true
+                partnership.disabled = true
+                tenten.disabled = true
+
+                drinksContainer.forEach(function(drinks) {
+                    if(drinks.children){
+                        Array.from(drinks.children).forEach(function(drink) {
+                            if(drink.children){
+                                Array.from(drink.children).forEach(function(d){
+                                    if(d.tagName == 'INPUT'){
+                                        if(d.parentElement.id != "coffee-container" && d.parentElement.id != "croissant-container" && d.parentElement.id != "iced-coffee-container" && d.parentElement.id != "chocolatine-container"){
+                                            d.disabled = true
+                                        }    
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }else {
+                company.disabled = false
+                delivery.disabled = false
+                partnership.disabled = false
+                tenten.disabled = false
+
+                drinksContainer.forEach(function(drinks) {
+                    if(drinks.children){
+                        Array.from(drinks.children).forEach(function(drink) {
+                            if(drink.children){
+                                Array.from(drink.children).forEach(function(d){
+                                    if(d.tagName == 'INPUT'){
+                                        d.disabled = false    
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+            break;
+        
+        case "10-10":
+            menuContainer.forEach(function(menus) {
+                Array.from(menus.children).forEach(function(menu) {
+                    if(menu.tagName == 'INPUT'){
+                        menu.disabled = element.checked
+                    }
+                    company.disabled = element.checked
+                    delivery.disabled = element.checked
+                    publicService.disabled = element.checked
+                    partnership.disabled = element.checked
+                })
             })
-        }else {
-            company.disabled = false
-            delivery.disabled = false
-            partnership.disabled = false
-            drinksContainer.forEach(function(drinks) {
-                if(drinks.children){
-                    Array.from(drinks.children).forEach(function(drink) {
-                        if(drink.children){
-                            Array.from(drink.children).forEach(function(d){
-                                if(d.tagName == 'INPUT'){
-                                    d.disabled = false    
-                                }
-                            })
-                        }
-                    })
-                }
-            })
-        }
+            break;
+        
+        default: break;
     }
     
     if(element.checked){
