1792688f95ef7a5e965dc67fda14113638008294
Azodox <likemoi99@gmail.com>
2022-12-04 15:50:39 +0100

    Check tenten

--- js/check.js
+++ js/check.js
@@ -4,7 +4,15 @@
  * And if you uncheck it, it will comeback to white.
  */
 
+const delivery = document.getElementById("delivery")
+const company = document.getElementById("company")
+
 function onCheck(element){
+    if(element.id === "10-10"){
+        company.disabled = element.checked
+        delivery.disabled = element.checked
+    }
+
     if(element.checked){
         document.getElementById("label-" + element.id).style.color = "#F3C4FB";
     }else{
